20d5116c81f45e2f9052d876852d0fd302e91c4b
curibe <uribeleon@gmail.com>
2021-11-24 23:30:07 -0500

    update deploy.sh and ci.yml

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,16 @@ BLUE='\e[34m'
 PURPLE='\e[35m'
 NC='\e[0m' # No Color
 
-echo -e "\n${BLUE}[✔] Doing something ${NC}"
+
+echo -e "\n${BLUE}[✔] Moving to folder's project ${NC}"
+cd /opt/test.actions
+ls
+
+echo -e "\n${BLUE}[✔] Stop supervisor ${NC}"
+supervisorctl stop test.actions
 
 echo -e "\n${BLUE}[✔] Pulling updates from the repo ${NC}"
+git pull origin master
 
 
 
