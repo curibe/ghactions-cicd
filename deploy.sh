84da79169386be488167e81799668b8bc5fc0c76
curibe <uribeleon@gmail.com>
2021-11-24 22:58:39 -0500

    update deploy.sh and ci.yml

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,9 @@ BLUE='\e[34m'
 PURPLE='\e[35m'
 NC='\e[0m' # No Color
 
-echo -e "\n${BLUE}[✔] Registering ssh key in ssh-agent ${NC}"
-eval $(ssh-agent -s)
-echo "${GITHUB_KEY}" | ssh-add -
+echo -e "\n${BLUE}[✔] Doing something ${NC}"
 
 echo -e "\n${BLUE}[✔] Pulling updates from the repo ${NC}"
-git pull origin master
+
 
 
